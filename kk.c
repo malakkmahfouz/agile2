23be4bcd82bb104d071144456bae1d2bbdecc0ef
malakkmahfouz <malakmahfouz@gmail.com>
2023-11-03 16:03:29 +0200

    version2

--- kk.c
+++ kk.c
@@ -2,8 +2,8 @@
 #ifndef BITWISE_OPERATION.H
 #define BITWISE_OPERATION.H
 
-#define Define_Bit( reg , bit) reg |= (1<<bit)
-#define Clear_Bit( reg ,  bit) reg &= ~(1<<bit)
+#define Set_Bit( reg , bit) reg |= (1<<bit)
+#define erase_Bit( reg ,  bit) reg &= ~(1<<bit)
 #define Get_Bit( reg ,  bit) reg &= (1<<bit)
 #define Toggle_Bit( reg , bit) reg ^= (1<<bit)
 
